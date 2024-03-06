c5c25cfea087dbc7bcc30e74d9f57e66de0f0a3a
Will Price <pricew86@gmail.com>
2024-03-06 16:12:19 +0800

    remove auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest } from "next/server";
+// import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
+// import { NextRequest } from "next/server";
 
-export default function middleware(req: NextRequest) {
-    return withAuth(req);
-}
-export const config = {
-    matcher: [ "/app/:path*"]
-};
+// export default function middleware(req: NextRequest) {
+//     return withAuth(req);
+// }
+// export const config = {
+//     matcher: [ "/app/:path*"]
+// };
